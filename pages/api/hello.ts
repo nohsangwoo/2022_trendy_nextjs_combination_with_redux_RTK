ca2a7df8315ebac87a4b5205612f2557e4d0062e
nohsangwoo <nsgr12@naver.com>
2022-10-10 19:25:17 +0900

    prisma usage

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import client from 'libs/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data>,
 ) {
+  const result = await client.user.findMany({
+    take: 10,
+  })
+
+  console.log('result: ', result)
   res.status(200).json({ name: 'John Doe' })
 }
